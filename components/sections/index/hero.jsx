debb427e92ed78001f60843240596bb63ca8f693
KevinVanHA <118561655+KevinVanHA@users.noreply.github.com>
2024-03-20 09:24:15 -0500

    FirstChanges

--- components/sections/index/hero.jsx
+++ components/sections/index/hero.jsx
@@ -65,11 +65,11 @@ export default function Hero() {
 				</section>
 				<section>
 					<button	className={`button ${button.primary}`}
-							onClick={ () => window.location = 'mailto:hello@andrewnelson.net' } >
+							onClick={ () => window.location = 'mailto:kevin@kevinvh.com' } >
 						{content.buttons.primary.title}
 					</button>
 					<button className={`button ${button.secondary} leaveSite`}
-							onClick={ ()=> window.open("https://www.linkedin.com/in/--andrewnelson/", "_blank") } >
+							onClick={ ()=> window.open("https://www.linkedin.com/in/kevin-van-haesendonck-82a6a7202/", "_blank") } >
 						{content.buttons.secondary.title}
 					</button>
 				</section>
